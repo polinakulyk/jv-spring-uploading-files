11a3e70a2b4e4be5f7832d9a406518a70f3f2c14
Polina Kulyk <polina.kulyk@protonmail.com>
2020-08-04 14:02:19 +0300

    Added functionality to handle file uploads

--- src/main/java/com/example/filesuploading/FilesUploadingApplication.java
+++ src/main/java/com/example/filesuploading/FilesUploadingApplication.java
@@ -1,13 +1,25 @@
 package com.example.filesuploading;
 
+import com.example.filesuploading.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class FilesUploadingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilesUploadingApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
